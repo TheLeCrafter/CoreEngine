3e59fd1e8aaac8d82c73db7a0242c850af0539ea
TheLeCrafter <47987588+TheLeCrafter@users.noreply.github.com>
2021-04-24 09:40:42 +0200

    Initial commit

---
+++ src/main/java/dev/thelecrafter/dimensionz/rpg/engine/Engine.java
@@ -0,0 +1,17 @@
+package dev.thelecrafter.dimensionz.rpg.engine;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Engine extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
