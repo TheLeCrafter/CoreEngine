afa793d5e686cff2a5e70eece1c1eae156e4c83d
TheLeCrafter <47987588+TheLeCrafter@users.noreply.github.com>
2021-05-01 21:48:00 +0200

    Debugging removed and improved some stuff

--- src/main/java/dev/thelecrafter/dimensionz/rpg/engine/utils/handlers/DamageStandsHandler.java
+++ src/main/java/dev/thelecrafter/dimensionz/rpg/engine/utils/handlers/DamageStandsHandler.java
@@ -23,25 +23,19 @@ public class DamageStandsHandler implements Listener {
     public void onDamage(EntityDamageEvent event) {
         if (!event.getEntity().getType().equals(EntityType.ARMOR_STAND)) {
             Random random = new Random();
-            int x = random.nextInt(6) + 3;
-            int z = random.nextInt(6) + 3;
-            System.out.println(1);
+            double x = random.nextInt(3) + 1.5;
+            double z = random.nextInt(3) + 1.5;
             ArmorStand stand = event.getEntity().getWorld().spawn(new Location(event.getEntity().getWorld(), 0, 0, 0), ArmorStand.class);
-            System.out.println(2);
-            stand.setMarker(true);
             stand.setInvisible(true);
+            stand.setMarker(true);
             stand.setInvulnerable(true);
-            System.out.println(3);
-            stand.setCustomName(ChatColor.RED + "" + event.getDamage());
+            stand.setCustomName(ChatColor.RED + "ᐊ " + event.getDamage() + " ᐅ");
             stand.setCustomNameVisible(true);
-            System.out.println(4);
+            stand.setGravity(false);
             stand.teleport(event.getEntity().getLocation().clone().add(x, event.getEntity().getHeight(), z));
-            System.out.println(5);
             stand.getPersistentDataContainer().set(TEMPORARY_STAND_KEY, PersistentDataType.STRING, "true");
             Bukkit.getScheduler().scheduleSyncDelayedTask(Engine.INSTANCE, () -> {
-                System.out.println(6);
                 if (!stand.isDead()) {
-                    System.out.println(7);
                     stand.remove();
                 }
             }, 3 * 20);
