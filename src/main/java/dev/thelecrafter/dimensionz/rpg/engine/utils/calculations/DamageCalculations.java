249be9501f89b462bbc680f4f6461022cd46ab18
TheLeCrafter <47987588+TheLeCrafter@users.noreply.github.com>
2021-05-01 15:47:40 +0200

    Fixed a bug with thousands of damage

--- src/main/java/dev/thelecrafter/dimensionz/rpg/engine/utils/calculations/DamageCalculations.java
+++ src/main/java/dev/thelecrafter/dimensionz/rpg/engine/utils/calculations/DamageCalculations.java
@@ -8,9 +8,10 @@ public class DamageCalculations {
     public static final NamespacedKey BASE_DAMAGE_KEY = new NamespacedKey(Engine.INSTANCE, "base_damage");
 
     public static double calculateWithStats(int baseDamage, int strength) {
-        // Formula base damage + (base damage * strength)
+        // Formula base damage + ((base damage * strength) / 100)
         double result = baseDamage;
         double multiplier = baseDamage * strength;
+        multiplier = multiplier / 100;
         result = result + multiplier;
         return result;
     }
