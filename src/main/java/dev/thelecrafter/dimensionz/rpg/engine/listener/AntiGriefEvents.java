1ae1713e34faac955228df4e669145ad55af9794
TheLeCrafter <47987588+TheLeCrafter@users.noreply.github.com>
2021-05-06 11:08:46 +0200

    Anti Grief Events

--- src/main/java/dev/thelecrafter/dimensionz/rpg/engine/listener/AntiGriefEvents.java
+++ src/main/java/dev/thelecrafter/dimensionz/rpg/engine/listener/AntiGriefEvents.java
@@ -2,6 +2,8 @@ package dev.thelecrafter.dimensionz.rpg.engine.listener;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerArmorStandManipulateEvent;
 
 public class AntiGriefEvents implements Listener {
@@ -9,6 +11,20 @@ public class AntiGriefEvents implements Listener {
     @EventHandler
     public void onArmorStand(PlayerArmorStandManipulateEvent event) {
         if (!event.getPlayer().hasPermission("coreengine.grief.armorstand")) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        if (!event.getPlayer().hasPermission("coreengine.grief.world.place")) {
+            event.setBuild(false);
+        }
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        if (!event.getPlayer().hasPermission("coreengine.grief.world.break")) {
             event.setCancelled(true);
         }
     }
